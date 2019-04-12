4b9228f0f3ed74b0549cebfd98c27b1de0b2a04f
Катя <1katiavolkova@gmail.com>
2019-04-12 19:13:20 +0300

    Исправила задание 2

--- index.php
+++ index.php
@@ -27,23 +27,23 @@ $user_name = 'Катя'; // укажите здесь ваше имя
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-        <?php if ($is_auth == 1) { ?>
-				<div class="user-menu__logged">
-					<p><?php echo($user_name) ?></p>
-					<a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
-					<a class="user-menu__logout" href="#">Выход</a>
-				</div>
-		<?php } else { ?>
-				<ul class="user-menu__list">
-					<li class="user-menu__item">
-					  <a href="#">Регистрация</a>
-					</li>
-					<li class="user-menu__item">
-					  <a href="#">Вход</a>
-					</li>
-				</ul>
-		<?php } ?>
+		
+		<?php if ($is_auth == 1) : ?>
+			<div class="user-menu__logged">
+				<p><?php echo($user_name) ?></p>
+				<a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+				<a class="user-menu__logout" href="#">Выход</a>
+			</div>
+		<?php else : ?>
+			<ul class="user-menu__list">
+				<li class="user-menu__item">
+					<a href="#">Регистрация</a>
+				</li>
+				<li class="user-menu__item">
+					<a href="#">Вход</a>
+				</li>
+			</ul>
+		<?php endif; ?>
 
         </nav>
     </div>
