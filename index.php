8ed4c1821a080287bcdc5765bc2c374dfa632252
Катя <1katiavolkova@gmail.com>
2019-04-15 20:51:59 +0300

    Итерация по массивам

--- index.php
+++ index.php
@@ -55,9 +55,20 @@ $user_name = 'Катя'; // укажите здесь ваше имя
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
+			<?php 
+			$categories = [
+			"Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"
+			];
+			$index = 0;
+			$num_count = count($categories);
+			while ($index < $num_count) {
+			?>
+			
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="pages/all-lots.html"><?= $categories[$index]; ?></a>
             </li>
+			<?php $index = $index + 1;
+			} ?>
         </ul>
     </section>
     <section class="lots">
@@ -66,17 +77,60 @@ $user_name = 'Катя'; // укажите здесь ваше имя
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+			<?php 
+			$lot = [
+			[
+			'title' => '2014 Rossignol District Snowboard',
+			'categories' => 'Доски и лыжи',
+			'price' => '10999',
+			'url_img' => 'img/lot-1.jpg'
+			],
+			[
+			'title' => 'DC Ply Mens 2016/2017 Snowboard',
+			'categories' => 'Доски и лыжи',
+			'price' => '159999',
+			'url_img' => 'img/lot-2.jpg'
+			],
+			[
+			'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+			'categories' => 'Крепления',
+			'price' => '8000',
+			'url_img' => 'img/lot-3.jpg'
+			],
+			[
+			'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+			'categories' => 'Ботинки',
+			'price' => '10999',
+			'url_img' => 'img/lot-4.jpg'
+			],
+			[
+			'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+			'categories' => 'Одежда',
+			'price' => '7500',
+			'url_img' => 'img/lot-5.jpg'
+			],
+			[
+			'title' => 'Маска Oakley Canopy',
+			'categories' => 'Разное',
+			'price' => '5400',
+			'url_img' => 'img/lot-6.jpg'
+			]
+			];
+			
+			foreach ($lot as $key => $item) {
+			?>
+					
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?= $item['url_img']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?= $item['categories']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['title']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $item['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -84,6 +138,7 @@ $user_name = 'Катя'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+			<?php }?>
         </ul>
     </section>
 </main>
@@ -93,9 +148,16 @@ $user_name = 'Катя'; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
+			<?php 
+			$index = 0;
+			while ($index < $num_count) {
+			?>
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?= $categories[$index]; ?></a>
             </li>
+			
+			<?php $index = $index + 1;
+			} ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
