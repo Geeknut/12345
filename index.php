b551f8a3da36559eedb22834f1ba0fcf859dcf60
Катя <1katiavolkova@gmail.com>
2019-04-15 23:13:42 +0300

    Итерация по массивам, правки 3

--- index.php
+++ index.php
@@ -3,44 +3,44 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Катя'; // укажите здесь ваше имя
 
-$lot = [
-			[
-			'title' => '2014 Rossignol District Snowboard',
-			'categories' => 'Доски и лыжи',
-			'price' => '10999',
-			'url_img' => 'img/lot-1.jpg'
-			],
-			[
-			'title' => 'DC Ply Mens 2016/2017 Snowboard',
-			'categories' => 'Доски и лыжи',
-			'price' => '159999',
-			'url_img' => 'img/lot-2.jpg'
-			],
-			[
-			'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-			'categories' => 'Крепления',
-			'price' => '8000',
-			'url_img' => 'img/lot-3.jpg'
-			],
-			[
-			'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-			'categories' => 'Ботинки',
-			'price' => '10999',
-			'url_img' => 'img/lot-4.jpg'
-			],
-			[
-			'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-			'categories' => 'Одежда',
-			'price' => '7500',
-			'url_img' => 'img/lot-5.jpg'
-			],
-			[
-			'title' => 'Маска Oakley Canopy',
-			'categories' => 'Разное',
-			'price' => '5400',
-			'url_img' => 'img/lot-6.jpg'
-			]
-			];
+$lots = [
+	[
+		'title' => '2014 Rossignol District Snowboard',
+		'categories' => 'Доски и лыжи',
+		'price' => '10999',
+		'url_img' => 'img/lot-1.jpg'
+	],
+	[
+		'title' => 'DC Ply Mens 2016/2017 Snowboard',
+		'categories' => 'Доски и лыжи',
+		'price' => '159999',
+		'url_img' => 'img/lot-2.jpg'
+	],
+	[
+		'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+		'categories' => 'Крепления',
+		'price' => '8000',
+		'url_img' => 'img/lot-3.jpg'
+	],
+	[
+		'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+		'categories' => 'Ботинки',
+		'price' => '10999',
+		'url_img' => 'img/lot-4.jpg'
+	],
+	[
+		'title' => 'Куртка для сноуборда DC Mutiny Charocal',
+		'categories' => 'Одежда',
+		'price' => '7500',
+		'url_img' => 'img/lot-5.jpg'
+	],
+	[
+		'title' => 'Маска Oakley Canopy',
+		'categories' => 'Разное',
+		'price' => '5400',
+		'url_img' => 'img/lot-6.jpg'
+	]
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
