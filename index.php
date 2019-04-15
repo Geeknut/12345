790cf96d6a1e63f228847aba049260568493654b
Катя <1katiavolkova@gmail.com>
2019-04-15 23:52:23 +0300

    Итерация по массивам, правки 6

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set ('display_errors', 1);
+ini_set ('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $is_auth = rand(0, 1);
 
 $user_name = 'Катя'; // укажите здесь ваше имя
@@ -110,19 +114,19 @@ $categories = ['Доски и лыжи', 'Крепления', 'Ботинки',
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-			<?php foreach ($lots as $key => $item) {?>
+			<?php foreach ($lots as $lot) : ?>
 					
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $item['url_img']; ?>" width="350" height="260" alt="">
+                    <img src="<?= $lot['url_img']; ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $item['categories']; ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $item['title']; ?></a></h3>
+                    <span class="lot__category"><?= $lot['categories']; ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lot['title']; ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= $item['price']; ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?= $lot['price']; ?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -130,7 +134,7 @@ $categories = ['Доски и лыжи', 'Крепления', 'Ботинки',
                     </div>
                 </div>
             </li>
-			<?php }?>
+			<?php endforeach;?>
         </ul>
     </section>
 </main>
