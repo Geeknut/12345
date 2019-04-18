c406f314ef55190b0c760e10ef770fbf84652bca
Катя <1katiavolkova@gmail.com>
2019-04-18 22:52:45 +0300

    Шаблонизация проекта, правки 1

--- index.php
+++ index.php
@@ -64,6 +64,12 @@ $title = 'Главная';
 
 $page_content = include_template('index.php',['lots' => $lots, 'categories' => $categories]);
 
-$layout_content = include_template('layout.php',['content' => $page_content, 'categories' => $categories, 'title' => $title, 'is_auth' => $is_auth, 'user_name' => $user_name]);
+$layout_content = include_template('layout.php',[
+	'content' => $page_content, 
+	'categories' => $categories, 
+	'title' => $title, 
+	'is_auth' => $is_auth, 
+	'user_name' => $user_name
+	]);
 
 print($layout_content);
