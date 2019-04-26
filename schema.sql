f5232165671ca2c4f2db2780461cc6d356372b40
Катя <1katiavolkova@gmail.com>
2019-04-26 13:55:26 +0300

    Создание схемы БД личного проекта, правки 2

--- schema.sql
+++ schema.sql
@@ -6,28 +6,33 @@ USE yeticave;
 CREATE TABLE category (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(128) NOT NULL UNIQUE,
-    symbolic_code VARCHAR(64) UNIQUE
+    code VARCHAR(64) UNIQUE
 );
 INSERT INTO category
-(title, symbolic_code) VALUES ('Доски и лыжи', 'boards'), ('Крепления', 'attachment'), ('Ботинки', 'boots'), ('Одежда', 'clothing'), ('Инструменты', 'tools'), ('Разное', 'other');
+(title, code) VALUES ('Доски и лыжи', 'boards'), ('Крепления', 'attachment'), ('Ботинки', 'boots'), ('Одежда', 'clothing'), ('Инструменты', 'tools'), ('Разное', 'other');
 
 CREATE TABLE lot (
     id INT AUTO_INCREMENT PRIMARY KEY,
     create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     title VARCHAR(128) NOT NULL,
-    description TEXT(500) NOT NULL,
+    description VARCHAR(500) NOT NULL,
     image VARCHAR(2083) NOT NULL,
     initial_price INT NOT NULL,
     end_time DATETIME NOT NULL,
     step_rate INT NOT NULL,
-    author_id INT NOT NULL,
-    winner_id INT NOT NULL,
+    user_id INT NOT NULL,
+    winner_id INT NULL,
     category_id INT NOT NULL
 );
 
+CREATE INDEX create_time ON lot(create_time);
+CREATE INDEX title ON lot(title);
+CREATE INDEX user_id ON lot(user_id);
+CREATE INDEX category_id ON lot(category_id);
+
 CREATE TABLE rate (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    rate_date TIMESTAMP NOT NULL,
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
     price INT NOT NULL,
     user_id INT NOT NULL,
     lot_id INT NOT NULL
@@ -35,12 +40,15 @@ CREATE TABLE rate (
 );
 
 CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    registration_date TIMESTAMP NOT NULL,
-    e_mail VARCHAR(320) NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT PRIMARY KEY ,
+    create_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+    email VARCHAR(320) NOT NULL UNIQUE,
     name VARCHAR(128) NOT NULL,
     password VARCHAR(64) NOT NULL,
-    user_img VARCHAR(2083) NULL,
-    contacts TEXT(500) NOT NULL
+    avatar VARCHAR(2083) NULL,
+    contacts VARCHAR(500) NOT NULL
 
 );
+
+CREATE INDEX create_time ON user(create_time);
+CREATE INDEX name ON user(name);
