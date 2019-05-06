429a07c375f54c02c65aa434b23b25cca90fcd70
Катя <1katiavolkova@gmail.com>
2019-05-06 22:45:10 +0300

    Работа со строкой запроса, правки 4

--- lot.php
+++ lot.php
@@ -12,27 +12,24 @@ $title = '';
 
 $connection = db_connect($config['db']);
 $categories = get_categories($connection);
-
 $id = $_GET['id'] ?? null;
+
 if (!$id) {
     die('Отсутствует обязательный параметр в запросе');
 }
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $lot = get_lot($connection, $id);
+$lot = get_lot($connection, $id);
 
-    if ($lot) {
-        $page_content = include_template('lot.php',[
-            'lot' => $lot,
-            'categories' => $categories
-        ]);
-    } else {
-        http_response_code(404);
-        $page_content = include_template('error.php',[
-            'categories' => $categories
-        ]);
-    }
+if ($lot) {
+    $page_content = include_template('lot.php',[
+        'lot' => $lot,
+        'categories' => $categories
+    ]);
+} else {
+    http_response_code(404);
+    $page_content = include_template('error.php',[
+        'categories' => $categories
+    ]);
 }
 
 $layout_content = include_template('layout.php',[
